a635985b1c33ad2a2eb0cc8644ce16c1391ff327
brandon chubb <brandon.chubb@kellogg.northwestern.edu>
2024-02-28 00:31:00 +0000

    almost done with conditionals

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -2,18 +2,31 @@
 # is the same as where this file is located and then run:
 # ruby 1-data.rb
 
-# In Ruby, there are different *types* of data:
+## In Ruby, there are different *types* of data:
 
-# Numbers
-
+# #Numbers
+# puts 5
+# puts 48
 # Non-numbers
+# puts 46 * 2
+# puts "tacos" +  3.to_s
 
-# True, False
+# #True, False
 
-# Nothing
+# #Nothing
 
 # Variables
+# tacos = 3
+# burritos = 2
+# puts tacos * burritos
 
-# Combine strings and variables
+## Combine strings and variables
 
-# String manipulation
+# first_name = "Brandon"
+# last_name = "Chubb"
+# email = "#{first_name} #{last_name}, nice to connect! Lmk when you're free next week!"
+# puts email
+
+
+## String manipulation
+puts "hello".swapcase
--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -2,14 +2,30 @@
 # is the same as where this file is located and then run:
 # ruby 2-conditionals.rb
 
-# Booleans
+##Booleans
+ # test_is_true = True
+# test_is_false = False
+# puts test_is_false 
 
-# Boolean Expressions
 
-# If Conditional Logic
+# # Boolean Expressions
+# puts 3 != 2
 
-# If/Else Conditional Logic
+# # If Conditional Logic
+# if 3 > 2 
+#     puts "Yessssssir!"
+# end
+
+
+# # If/Else Conditional Logic
+# if 3 > 2 
+#     puts "Yessssssir!"
+# else 
+#     puts "That aint P!!!!"
+# end
+
 
 # Elsif Conditional Logic
+
 
 # Combining Expressions
